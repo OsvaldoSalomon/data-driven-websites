5292c03c8a013eff731b0476dc47c06cb4ff398e
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2022-04-28 13:09:52 -0400

    Finished Part 1

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//     res.render('index', { title: 'Home' });
-// });
-
-router.get('/', (req, res) => {
-    throw new Error('This is a test error!');
+router.get("/", (req, res) => {
     res.render('index', { title: 'Home' });
 });
+
+// router.get('/', (req, res) => {
+//     throw new Error('This is a test error!');
+//     res.render('index', { title: 'Home' });
+// });
 
 module.exports = router;
