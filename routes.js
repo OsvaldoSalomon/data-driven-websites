98b5f4c1d82f5f3f95b00033c5865f56860771f4
connorwfitch <connorwfitch@gmail.com>
2022-04-28 12:58:54 -0400

    added custom errors

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+// router.get("/", (req, res) => {
+//     res.render('index', { title: 'Home' });
+// });
+
+router.get('/', (req, res) => {
+    throw new Error('This is a test error!');
     res.render('index', { title: 'Home' });
 });
 
